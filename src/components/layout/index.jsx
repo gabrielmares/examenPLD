6e5d42d3f0540ea4000b5909d16d85b0321e7f1d
Gabriel Mares <43484502+gabrielmares@users.noreply.github.com>
2020-08-01 01:43:19 -0700

    header con context para manejo de las respuestas del examen

--- src/components/layout/index.jsx
+++ src/components/layout/index.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Header from './Header';
-import Examen from '../Exam';
+import Examen from '../privates/Exam';
+import HandleQuestion from "../../provider/contextAnswers";
 
+const LayOut = ({ usuario }) => {
 
-const LayOut = ({usuario}) => {
-
+    // const {  } = React.useContext(respuestasContext);
     return (
         <><HandleQuestion>
             <Header usuario={usuario} />
