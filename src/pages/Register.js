4a905865861414ee80e10ee1cb9ae6c58e1fa10e
Gabriel Mares <43484502+gabrielmares@users.noreply.github.com>
2020-08-22 22:49:06 -0700

    se agrego el modal para la interaccion con las acciones en la lista de usuarios

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -3,27 +3,26 @@ import ListUserCard from '../components/privates/ListUsers';
 import SignIn from '../components/signin/SignIn';
 import RegisterContext from '../provider/contextRegister';
 import Header from '../components/layout/Header'
-
+import { Row } from 'reactstrap'
+import ModalDialog from '../components/privates/Modal'
 
 // const NewListCard = React.lazy(() => import('../components/privates/ListUsers'));
 
 const Registerpage = () => {
 
     return (
-        <>
-            <RegisterContext>
-                <Header />
-                <div className="row p-4">
-                    <div className="offset-2 col-4">
-                        <SignIn />
-                    </div>
-                    <div className="col-4">
-                        <ListUserCard />
-                    </div>
+        <RegisterContext>
+            <Header />
+            <Row className="col-12 mt-4">
+                <div className="offset-2 col-4">
+                    <SignIn />
                 </div>
-
-            </RegisterContext>
-        </>
+                <div className="col-4">
+                    <ListUserCard />
+                </div>
+            </Row>
+            <ModalDialog />
+        </RegisterContext>
 
     );
 }
